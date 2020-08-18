471147ac14ae5ab0339631eea5447ccc6d8e1fd1
s0567271 <s0567271@htw-berlin.de>
2020-08-18 19:48:26 +0200

    adding project

---
+++ WarCardGame/WarCardGame/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by John Waluya on 24.03.20.
+//  Copyright © 2020 John Waluya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func DealTapped(_ sender: Any) {
+    
+        // Randomize some numbers
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        // Update the Image Views
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        // Compare the random numbers
+        if leftNumber > rightNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        
+        else if leftNumber < rightNumber {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+        else {
+            
+        }
+    
+    }
+    
+
+}
+
